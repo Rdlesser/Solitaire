bb50ffe96941341915a81e57974d3fd13a7a97eb
Robert Lesser <rdlesser@gmail.com>
2024-09-18 14:06:29 +0300

    Generate deck method

--- Assets/Scripts/Solitaire.cs
+++ Assets/Scripts/Solitaire.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Solitaire : MonoBehaviour
 {
+    public static string[] Suits = new string[] { "C", "D", "H", "S" };
+    public static string[] Values = new[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    public List<string> _deck;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayCards();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayCards()
+    {
+        _deck = GenerateDeck();
+        
+        //test the cards in the deck:
+        foreach (var card in _deck)
+        {
+            Debug.Log(card);
+        }
+    }
+
+    public static List<string> GenerateDeck()
+    {
+        List<string> newDeck = new List<string>();
+
+        foreach (var suit in Suits)
+        {
+            foreach (var value in Values)
+            {
+                newDeck.Add(suit + value);
+            }
+        }
+
+        return newDeck;
     }
 }
