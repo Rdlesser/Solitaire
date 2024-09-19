a6d66fc36e82ad9ce71175b63bd41c1c096f9f1d
Robert Lesser <rdlesser@gmail.com>
2024-09-19 11:02:13 +0300

    code cleanup

--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using DefaultNamespace;
 using UnityEngine;
 
@@ -12,20 +11,10 @@ public class Selectable : MonoBehaviour
 
     public Action OnFaceUpUpdated;
 
-    public bool IsTop
-    {
-        get => _isTop;
-        set => _isTop = value;
-    }
-
+    public bool IsTop { get; set; }
     public string Suit { get; set; }
     public int Value { get; set; }
-
-    public int Row
-    {
-        get => _row;
-        set => _row = value;
-    }
+    public int Row { get; set; }
 
     public bool IsFaceUp
     {
@@ -44,39 +33,31 @@ public class Selectable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (CompareTag(Tags.CARD))
+        if (!CompareTag(Tags.CARD))
         {
-            Suit = transform.name[0].ToString();
-
-            for (int i = 1; i < transform.name.Length; i++)
-            {
-                var character = transform.name[i];
-                _valueString += character;
-            }
-
-            Value = _valueString switch
-            {
-                "A" => 1,
-                "2" => 2,
-                "3" => 3,
-                "4" => 4,
-                "5" => 5,
-                "6" => 6,
-                "7" => 7,
-                "8" => 8,
-                "9" => 9,
-                "10" => 10,
-                "J" => 11,
-                "Q" => 12,
-                "K" => 13,
-                _ => throw new ArgumentOutOfRangeException()
-            };
+            return;
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        Suit = transform.name.FirstOrDefault().ToString();
+        _valueString = transform.name[1..];
         
+
+        Value = _valueString switch
+        {
+            "A" => 1,
+            "2" => 2,
+            "3" => 3,
+            "4" => 4,
+            "5" => 5,
+            "6" => 6,
+            "7" => 7,
+            "8" => 8,
+            "9" => 9,
+            "10" => 10,
+            "J" => 11,
+            "Q" => 12,
+            "K" => 13,
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 }
