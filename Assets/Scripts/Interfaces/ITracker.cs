f737a237a9c9c7c1af7fd73cc31faf01cdd52c52
Robert Lesser <rdlesser@gmail.com>
2024-09-19 13:45:17 +0300

    event tracking

---
+++ Assets/Scripts/Interfaces/ITracker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface ITracker
+    {
+        void TrackEvent(string eventName, Dictionary<string, string> eventParams);
+    }
+}
---
+++ Assets/Scripts/SimpleEventTracker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Interfaces;
+using UnityEngine;
+
+public class SimpleEventTracker : ITracker
+{
+    public void TrackEvent(string eventName, Dictionary<string, string> eventParams)
+    {
+        Debug.Log($"Event: {eventName}, Params: {string.Join(", ", eventParams.Select(kv => kv.Key + "=" + kv.Value))}");
+    }
+}
